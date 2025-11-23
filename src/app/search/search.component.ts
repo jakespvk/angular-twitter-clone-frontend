cac5d6c89444c41a4bdaf4b13ce83b7434eceda6
Jake Spievak <jakespvk@gmail.com>
2025-11-22 16:24:23 -0800

    add input component and make searching realllyyyyyyyyyy nice

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,19 @@
 import { Component, inject } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
 import { TweetsService } from '../tweets.service';
 import { Tweet } from '../tweet';
 import { TweetComponent } from '../tweet/tweet.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ZardButtonComponent } from '@shared/components/button/button.component';
+import { ZardIconComponent } from '@shared/components/icon/icon.component';
+import { ZardInputDirective } from '@shared/components/input/input.directive';
 
 @Component({
+  standalone: true,
   selector: 'app-search',
-  imports: [TweetComponent, ReactiveFormsModule, RouterLink],
+  imports: [TweetComponent, ReactiveFormsModule, RouterLink, ZardButtonComponent, ZardIconComponent, ZardInputDirective],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
@@ -17,24 +22,24 @@ export class SearchComponent {
   tweetsService: TweetsService = inject(TweetsService);
   filteredTweetList: Tweet[] = [];
 
+  searchTerm = new FormControl('');
+
   constructor() {
     this.tweetsService.getAllTweets().then((tweetList: Tweet[]) => {
       this.tweetList = tweetList;
       this.filteredTweetList = tweetList;
     });
+
+    this.searchTerm.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((searchTerm: string | null) => {
+        this.tweetsService.getFilteredTweets(searchTerm ?? "").then((filteredTweetList: Tweet[]) => {
+          this.filteredTweetList = filteredTweetList;
+        });
+      });
   }
 
-  filterResults(text: string) {
-    if (!text) {
-      this.filteredTweetList = this.tweetList;
-      return;
-    }
-
-    // this.filteredTweetList = this.tweetList.filter((tweet) =>
-    //   tweet?.message.toLowerCase().includes(text.toLowerCase()),
-    // );
-    this.tweetsService.getFilteredTweets(text).then((filteredTweetList: Tweet[]) => {
-      this.filteredTweetList = filteredTweetList;
-    });
+  setFocused(element: HTMLElement) {
+    element.focus();
   }
 }
